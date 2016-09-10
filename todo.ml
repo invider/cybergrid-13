9a88b63bc35f3e067f6a3c98d9ddcfc83b367381
Igor Khotin <khotin@gmx.com>
2016-09-10 15:34:43 +0300

    [*] updated todo

--- todo.ml
+++ todo.ml
@@ -1,11 +1,10 @@
-> refactor blocks to entities model
 > minimal mat4 reimplementation
 > glitch animated texture effect
 > neon blending effect
 > map/path generator
 > render cooling
-
 ------------------------------------------
 V objects independent translation/rotation
 V signals
 V semi-transparency in generated textures
+V refactor blocks to entities model
